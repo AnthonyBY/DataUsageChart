5fbc653881dce425b215cae01c413a96261a8aa2
Anton Marchanka <anthonymarchenko@gmail.com>
2026-02-25 11:46:48 +0700

    Initial Commit

---
+++ DataUsageChart/DataUsageChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataUsageChartApp.swift
+//  DataUsageChart
+//
+//  Created by Anton Marchanka on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct DataUsageChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
